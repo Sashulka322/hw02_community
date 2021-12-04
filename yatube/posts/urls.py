3e55f89954fdc267bf9f03f49a1dec454954ff0f
Alexandr <gribov.sasha2017@yandex.ru>
2021-12-04 09:42:10 +0300

    First_commit

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    #Главная страница
+    path('', views.index,  name='index'),
+    path('group/',views.posts_friends, name='posts_friends'),
+    #Группа
+    path('group/<slug:anyslug>/',views.posts_all, name='posts_all'),
+]
