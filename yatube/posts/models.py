eec72767047dd18e883e1aa465ca051d1d03a2d7
Alexandr <gribov.sasha2017@yandex.ru>
2021-12-08 23:10:16 +0300

    Fix_all_1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+
+class Group(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='Group'
+    )
