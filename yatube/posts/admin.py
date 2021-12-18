0a52ee64d8656a4e15ed4c68427b87b796ea2dc3
Alexandr <gribov.sasha2017@yandex.ru>
2021-12-18 11:41:56 +0300

    Final_fix_rev_2

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -12,16 +12,11 @@ class GroupAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     list_editable = ('group',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 
 
 admin.site.register(Post, PostAdmin)
