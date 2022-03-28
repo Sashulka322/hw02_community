855099f36028abd4d12afed5a2759559c60b1214
Alexandr <gribov.sasha2017@yandex.ru>
2022-03-29 01:06:32 +0300

    error post_detail

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_posts'),
     path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:post_pk>/', views.post_detail, name='post_detail'),
 
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,11 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
 
-from .models import Post, Group
+from .models import Post, Group, User
 
+
+User = get_user_model()
 
 def index(request):
     post_list = Post.objects.all().order_by('-pub_date')
@@ -25,31 +28,28 @@ def group_posts(request, slug):
     }
     return render(request, 'posts/group_list.html', context)
 
+
 def profile(request, username):
     user = get_object_or_404(User, username=username)
-    post_list = Post.object.filter(author=user)
+    post_list = Post.objects.filter(author=user)
     paginator = Paginator(post_list, 10) 
-    page_namber =  request.GET.get('page')
-    page_obj = paginator.get_page(page_namber)
+    page_number =  request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     posts_count = Post.objects.filter(author=user).count()
     context = {
         'user': user,
         'posts_list': post_list,
         'posts_count': posts_count,
-        'page_obj': page_obj
+        'page_obj': page_obj,
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    posts_count = Post.objects.filter(author=post_author).count()
+    post = get_object_or_404(Post, id=post_id)
+    post_count = Post.objects.filter(author=post.author).count()
     context = {
         'post': post,
-        'posts_count': posts_count
+        'post_count': post_count,
     }
     return render(request, 'posts/post_detail.html', context)
-
-
-
-    
